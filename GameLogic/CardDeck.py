ae00da4b79e50a00dffbf3f9c0f22b7d2205b0a0
VictorCeobanu <daty1000@gmail.com>
2020-03-28 13:41:01 +0200

    Hand Evaluator

--- GameLogic/CardDeck.py
+++ GameLogic/CardDeck.py
@@ -5,17 +5,22 @@ import numpy as np
 class CardDeck:
 
     def __init__(self):
-        self.cardNumbers = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+        self.cardNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+        self.cardNumbersEvaluation = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
         self.cardColors = ['♡', '♤', '♧', '♢']
+        self.cardColorsEvaluation = ['heart', 'spade', 'club', 'diamond']
         self.deckDefinition = dict()
+        self.deckDefinitionEvaluation = dict()
 
         i = 0
         for color in range(len(self.cardColors)):
             for card in range(len(self.cardNumbers)):
                 self.deckDefinition[i] = f'{self.cardNumbers[card]}{self.cardColors[color]}'
+                self.deckDefinitionEvaluation[i] = (self.cardNumbersEvaluation[card],self.cardColorsEvaluation[color])
                 i += 1
 
         print(self.deckDefinition)
+        print(self.deckDefinitionEvaluation)
 
     # create an array of 52 cards(as indexes) and shuffle it
     def getShuffledDeck(self):
@@ -26,6 +31,9 @@ class CardDeck:
     # get the card name/color based on input index
     def getCardNumberTranlation(self, index):
         return self.deckDefinition[index]
+
+    def getCardNumberTranlationEvaluation(self, index):
+        return self.deckDefinitionEvaluation[index]
 
     def printShuffledDeck(self, shuffledDeckIndexes):
         prnt = 'Shuffled Deck: '
